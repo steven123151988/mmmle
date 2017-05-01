519e4a54c20cc1da414cf21293d9fc247461fd7c
DESKTOP-4BH5UTJ\18 <steven123151988@gmailcom>
2017-05-01 13:03:46 +0800

    测试OK

--- lottery/app/src/main/java/com/daking/lottery/application/MyActivityManager.java
+++ lottery/app/src/main/java/com/daking/lottery/application/MyActivityManager.java
@@ -24,7 +24,6 @@ public class MyActivityManager {
             currentActivity = sCurrentActivityWeakRef.get();
         }
         return currentActivity;
-@##
     }
 
     public void setCurrentActivity(Activity activity) {
